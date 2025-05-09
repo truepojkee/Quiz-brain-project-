2ecb1632f88cb7398111eeae5c3a48b4cbdb7248
truepojkee <78331998+truepojkee@users.noreply.github.com>
2025-05-09 09:48:44 +0200

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,14 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+# new_q = Question("2+2 is 4?", "True")
+# print(new_q.text)
+# print(new_q.answer)
+
+
+
+
+
